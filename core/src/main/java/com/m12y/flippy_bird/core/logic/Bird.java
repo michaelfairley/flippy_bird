b1de5a891e42f01f3d1ed131bf7f48a2d8318ef5
Michael Fairley <michaelfairley@gmail.com>
2014-02-23 23:39:08 -0600

    Rename MAX_VELOCITY to TERMINAL_VELOCITY

--- core/src/main/java/com/m12y/flippy_bird/core/logic/Bird.java
+++ core/src/main/java/com/m12y/flippy_bird/core/logic/Bird.java
@@ -11,8 +11,8 @@ public class Bird {
     public static final float WIDTH = 0.9f;
     public static final float HEIGHT = 0.8f;
     private static final float GRAVITY = 0.012f;
-    private static final float MAX_VELOCITY = 0.23f;
-    private static final float DRAG_COEFFICIENT = GRAVITY / MAX_VELOCITY;
+    private static final float TERMINAL_VELOCITY = 0.23f;
+    private static final float DRAG_COEFFICIENT = GRAVITY / TERMINAL_VELOCITY;
 
     public Bird() {
         position = Game.HEIGHT/2;
